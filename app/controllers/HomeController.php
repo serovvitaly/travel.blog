e086c9b5df5a94f672c703dc2ffc5547053f3fbc
Vitaliy Serov <serovvitaly@gmail.com>
2013-06-05 11:55:38 +0400

    коммит

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,10 +2,17 @@
 
 class HomeController extends BaseController {
 
-	protected $layout = 'base.layout';
+	protected $layout = 'base.layout.column1';
 
 	public function getIndex()
 	{
+        
+        $posts = BlogPost::take(20)->get();
+        
+        foreach ($posts as $post) {
+            echo '<p>'.$post->title.'</p>';
+        }
+        
 		$this->layout->content = 300;
 	}
 
---
+++ app/models/BlogPost.php
@@ -0,0 +1,5 @@
+<?php
+
+class BlogPost extends Eloquent {
+
+}
